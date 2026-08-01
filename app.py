4f8169fc7b3f0e6302c9245274ef96ed5acdf0fa
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2026-08-01 05:10:40 +0330

    updated artifacts

--- app.py
+++ app.py
@@ -894,7 +894,7 @@ def predict_failure_risk(features, dataset, artifacts):
         elif h in tuned_thresholds:
             threshold = tuned_thresholds[h]['XGBoost']
         else:
-            threshold = 0.05
+            threshold = 0.01
 
         risks[f'h{h}'] = {
             'probability': prob,
