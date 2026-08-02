9e68794547c6ea51e61ba4638acaa5f84383fdcd
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2026-08-02 07:48:02 +0330

    debugged UI

--- app.py
+++ app.py
@@ -63,10 +63,10 @@ st.markdown("""
     # .stSlider > div > div > div > div[data-baseweb="slider"] {
     #     background: linear-gradient(90deg, #1db954, #1ed760) !important;
     # }
-    # .stSlider > div > div > div > div > div[role="slider"] {
-    #     background-color: #1db954 !important;
-    #     border-color: #1db954 !important;
-    # }
+    .stSlider > div > div > div > div > div[role="slider"] {
+        background-color: #1db954 !important;
+        border-color: #1db954 !important;
+    }
     # .stSlider > div > div > div > div > div[role="slider"]:focus-visible {
     #     outline: 2px solid rgba(29, 185, 84, 0.5) !important;
     #     outline-offset: 2px !important;
