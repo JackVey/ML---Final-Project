ce9468e9cad30fd0096bd062324327c722f5771b
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2026-08-02 07:42:06 +0330

    debugged UI

--- app.py
+++ app.py
@@ -57,11 +57,7 @@ st.markdown("""
     .stSlider > div > div > div {
         background: linear-gradient(90deg, #1db954, #1ed760) !important;
     }
-    .stSlider > div > div > div > div {
-        # background-color: #1db954 !important;
-        border: 2px solid #1db954 !important;
-    }
-    # tick number color
+    
     .stSlider > div > div > div > div > div {
         color: #1db954 !important;
     }
